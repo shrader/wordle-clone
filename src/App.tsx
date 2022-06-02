722e271ec0403926231a7b7d5e8bc611074c414f
John Shrader <figurefactory@gmail.com>
2022-06-01 23:25:07 -0400

    fixed splitting event listener and fetch up

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,15 @@
-import React, {useEffect, useState, useCallback} from 'react';
+import React, {useEffect, useState} from 'react';
 import * as API from './Api';
 import * as type from './types';
 import * as helpers from './helpers';
-import WordDisplay from './WordDisplay';
+// import WordDisplay from './WordDisplay';
 import './App.css';
 
-function App() {
-  const [answer, setAnswer] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
+function GameContainer({answer}:{answer:string}) {
   const [guesses, setGuesses] = useState<type.WordGuess[]>([]);
   const [testChars, setTestChars] = useState("");
-  
-  const getAndSetNewWord = useCallback(async () => {
-    
-    try {
-      const newWord = await API.getNewWord();
-      setAnswer(newWord.toUpperCase());
-      setIsLoading(false);
-      console.log('answer!!! ', answer);
-    } catch(e) {
-      console.error(e);
-      setAnswer('START'); // start is just for testing when/if getNewWord fails
-      setIsLoading(false); 
-    }
-  }, [])
 
-  function SubApp({answer}:{answer:string}) {
-
-    useEffect(() => {
-      if (!isLoading) {
-        console.log("answer: ", answer);
-      } else {
-        console.log('still loading...');
-      }
-  
-      document.addEventListener('keydown', handleKeyEvent);   
-  
-      return function cleanup() {
-        console.log("UNMOUNTING: removing event listener");
-        window.removeEventListener("keydown", handleKeyEvent);
-        };
-    
-    } ,[])
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          {isLoading? <p>Loading...</p>:<GameContainer />}
-        </header>
-      </div>
-    );
-  }
-
-
-  function GameContainer() {
-
-    function WordDisplayContainer() {
-      return (
-        <>
-        {guesses.map(guess => {
-          <GuessDisplay guess={guess} />
-        })}
-        </>
-      );
-    }
-  
-    interface GuessDisplayPropa {
-      guess: type.WordGuess
-    }
-  
-    function GuessDisplay({guess}:GuessDisplayPropa) {
-      return (
-        <p>
-         {guess.map(guess => {
-          return <span style={{backgroundColor:guess.color, padding:'5px'}}>{guess.letter}</span>
-        })}
-        </p>
-      )
-    }
-
-    return (
-      <>
-        <WordDisplayContainer />
-        <WordDisplay words={testChars} />
-      </>
-    )
-  }
-
-  let tempStr = ""
+  let tempStr = "";
 
   function handleKeyEvent(event: KeyboardEvent) {
     const key = event.key.toUpperCase();
@@ -117,16 +39,15 @@ function App() {
     return
   }
 
-  // const memoizedHandleKeyEvent = useCallback(handleKeyEvent, [answer]);
+  const mapGuesses = (guessArr:type.WordGuess[]) => {
+    guessArr.map(guess => {
+      return guess.map(props => {
+        return <span style={{backgroundColor:props.color, padding:10}}>{props.letter}</span>
+      })
+    })
+  }
 
   useEffect(() => {
-    getAndSetNewWord();
-    setTimeout(()=>{},10000);
-    if (!isLoading) {
-      console.log("answer: ", answer);
-    } else {
-      console.log('still loading...');
-    }
 
     document.addEventListener('keydown', handleKeyEvent);   
 
@@ -135,11 +56,43 @@ function App() {
       window.removeEventListener("keydown", handleKeyEvent);
       };
   
-  } ,[getAndSetNewWord])
+  } ,[])
 
-  console.log("answer: ", answer);
+  return (
+    <>
+    {testChars}
 
-  return <SubApp answer={answer} />
+    </>
+  );
+}
+
+function App() {
+  const [answer, setAnswer] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getAndSetNewWord = async () => {
+      try {
+        const newWord = await API.getNewWord();
+        setAnswer(newWord.toUpperCase());
+        setIsLoading(false);
+      } catch(e) {
+        console.error(e);
+        setAnswer('START'); // start is just for testing when/if getNewWord fails
+        setIsLoading(false); 
+      }
+    }
+
+    getAndSetNewWord();
+  } ,[])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {isLoading? <p>Loading...</p>:<GameContainer answer={answer}/>}
+      </header>
+    </div>
+  );
 
 }
 
