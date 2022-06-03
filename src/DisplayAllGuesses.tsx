5701b6e2cc40d72168fee05ed5a4b058735e2164
John Shrader <figurefactory@gmail.com>
2022-06-03 14:13:37 -0400

    seperated components

--- src/DisplayAllGuesses.tsx
+++ src/DisplayAllGuesses.tsx
@@ -1,21 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import * as type from './types';
 import { v4 as uuidv4 } from 'uuid';
-
-function SingleGuess({guess}:{guess:type.WordGuess}) {
-  
-  // return <span key={uuidv4()} style={{backgroundColor:props.color, padding:10}}>{props.letter}</span>
-  return (
-    <>
-    {guess.map(props => {
-      return (
-        <span key={uuidv4()} style={{backgroundColor:props.color}}>
-        {props.letter}
-      </span>
-      );})}
-      </>
-  );
-}
+import SingleGuess from './SingleGuess';
 
 function DisplayAllGuesses({guesses}:{guesses:type.WordGuess[]}) {
 
